194802a6e939d941392ff00244a4a293ee872b90
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-02-16 15:30:13 -0300

    Adiciona Requisitos de 1 a 4

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Referencias de páginas usadas como pesquisa.
+  https://www.w3schools.com/css/css3_buttons.asp
+
+
+
+*/
+
+function addNewLi(){
+  let positionButton = document.querySelector('#criar-tarefa');
+}
